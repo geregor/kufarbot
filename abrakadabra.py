2e0d946882be7e4236de5131653446785f9f382d
geregor <52245105+geregor@users.noreply.github.com>
2021-02-14 14:13:29 +0300

    Add files via upload

---
+++ abrakadabra.py
@@ -0,0 +1,208 @@
+from typing import re
+import requests
+from bs4 import BeautifulSoup as BS
+from fake_useragent import UserAgent
+import re
+import telebot
+import keyboards as kb
+from telebot import types
+import sqlite3
+mass = []
+massiv = []
+pos = {}
+conn = sqlite3.connect('kufar.db')
+bot = telebot.TeleBot("1685702621:AAGr22uWJGIgC-40uVp3q0ayfpxpFNgLy1Q")
+message_id = 0
+@bot.message_handler ( commands=[ 'start' ] )
+def bb(message):
+    user_id = message.chat.id
+    with sqlite3.connect ( 'kufar.db' ) as conn :
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT reg FROM sett WHERE user_id = {user_id}")
+        qq = cursor.fetchone()
+        if qq == None:
+            msg = bot.send_message(user_id, 'Привет мой друг, я Kufar бот. Для дальнейшего пользования ботом введите ключ', reply_markup=kb.dbut)
+            bot.register_next_step_handler ( msg , second_room )
+        else:
+            msg = bot.send_message(user_id, "С возвращением!", reply_markup=kb.main)
+            bot.register_next_step_handler(msg, main_room)
+
+def second_room(message):
+    user_id = message.chat.id
+    text = message.text
+    if text == 'У меня нету ключа':
+        msg = bot.send_message(user_id,"Обратитесь к создателю бота!")
+        bot.register_next_step_handler(msg, second_room)
+    elif text == 'Пакепачи':
+        msg = bot.send_message(user_id,"Ключ верный, приятного использования.", reply_markup=kb.main)
+        bot.register_next_step_handler(msg, main_room)
+        with sqlite3.connect ( 'kufar.db' ) as conn :
+            cursor = conn.cursor ()
+            cursor.execute ( f"SELECT user_id FROM sett WHERE user_id={user_id}" )
+            if cursor.fetchone () == None :
+                cursor.execute ( f"INSERT INTO sett(user_id,reg) VALUES({user_id},1)" )
+                conn.commit ()
+    else:
+        msg = bot.send_message(user_id,"Неверный ключ!")
+        bot.register_next_step_handler(msg, second_room)
+
+def main_room(message):
+    user_id = message.chat.id
+    text = message.text
+    if text == "Найти халяву":
+        msg = bot.send_message(user_id, "Выбери категорию для халявы", reply_markup=kb.first_but)
+        bot.register_next_step_handler(msg, find_1)
+    elif text == "Ожидание халявы":
+        msg = bot.send_message(user_id, "В разработке", reply_markup=kb.main)
+        bot.register_next_step_handler(msg, main_room)
+        #msg = bot.send_message(user_id, "Выберите категорию.", reply_markup=kb.fisrt_but)
+        #bot.register_next_step_handler(msg, waiting)
+    else:
+        msg = bot.send_message(user_id, "Я не понимаю, используйте клавиатуру")
+        bot.register_next_step_handler(msg, main_room)
+
+def find_1(message):
+    user_id = message.chat.id
+    text = message.text
+    with sqlite3.connect ('kufar.db') as conn :
+        cursor = conn.cursor ()
+        c = False
+        if text == "Легкая техника":
+            cursor.execute(f"UPDATE sett SET find = 1 WHERE user_id = {user_id}")
+            c = True
+        elif text == "Тяжелая техника":
+            cursor.execute(f"UPDATE sett SET find = 2 WHERE user_id = {user_id}")
+            c = True
+        elif text == "Одежда(жен)":
+            cursor.execute(f"UPDATE sett SET find = 3 WHERE user_id = {user_id}")
+            c = True
+        elif text == "Приставки":
+            cursor.execute(f"UPDATE sett SET find = 4 WHERE user_id = {user_id}")
+            c = True
+        elif text == "Вернуться в меню":
+            msg = bot.send_message(user_id, "Вы вернулись в меню.", reply_markup=kb.main)
+            bot.register_next_step_handler(msg,main_room)
+        else:
+            msg = bot.send_message(user_id, "Я не понимаю, используйте клавиатуру.")
+            bot.register_next_step_handler(msg, find_1)
+        if c == True:
+            conn.commit()
+            msg = bot.send_message ( user_id , f"Вы выбрали '{text}'\nВыберите ценовой промежуток, пример 10-40, пробелы и 'Р' не нужны. Можно просто 40." )
+            bot.register_next_step_handler(msg, seach_toom)
+
+def seach_toom(message):
+    user_id = message.chat.id
+    text = message.text
+    text = text.replace(' ', '')
+    start = 0
+    end = 100
+    c = False
+    try:
+        if re.search(r'-', text) != None:
+            start = re.split(r'-', text)[0]
+            end = re.split(r'-', text)[1]
+        if int(end) > int(start):
+            msg = bot.send_message(user_id, "Вы выбрали промежуток "+str(start)+" - "+str(end)) #,reply_markup=kb.ww)
+            with sqlite3.connect ( 'kufar.db' ) as conn :
+                cursor = conn.cursor ()
+                cursor.execute(f"UPDATE sett SET money = '{start}-{end}' WHERE user_id = {user_id}")
+                conn.commit()
+            #bot.register_next_step_handler(msg, toom)
+            c = True
+        else:
+            msg = bot.send_message(user_id, "Выбран не верный промежуток, 2-ое число должно быть больше 1-ого!")
+            bot.register_next_step_handler(msg, seach_toom)
+
+    except:
+        msg = bot.send_message(user_id, "Произошло недопонимание №100")
+        bot.register_next_step_handler(msg, seach_toom)
+
+#def toom(message):
+    if c == True:
+        user_id = message.chat.id
+        text = message.text
+        with sqlite3.connect ( 'kufar.db' ) as conn :
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT find FROM sett WHERE user_id = {user_id}")
+            qq = cursor.fetchone()
+            for a in qq:
+                kom = a
+            cursor.execute(f"SELECT money FROM sett WHERE user_id = {user_id}")
+            qq = cursor.fetchone()
+            for a in qq:
+                sum = a
+        print(sum)
+        sum = re.split(r'-', sum)
+        if kom == 1:
+            link = 'https://baraholka.onliner.by/search.php?type=lastposts&time=86400&r=1&cat=1'
+        elif kom == 2:
+            link = 'https://baraholka.onliner.by/search.php?type=lastposts&time=86400&r=2&cat=1'
+        elif kom == 3:
+            link = 'https://baraholka.onliner.by/search.php?type=lastposts&time=86400&r=571&cat=1'
+        elif kom == 4:
+            link = 'https://baraholka.onliner.by/search.php?type=lastposts&time=86400&r=4&cat=1'
+        r = requests.get(link)
+        soup = BS ( r.content , 'html.parser' )
+
+        for i in soup.findAll ( 'div' , class_='m-title' ) :
+            b = i.text
+            b = b.replace ( 'Новое за 24 часа(найдено ' , '' )
+            b = b.replace ( ' объявлений)' , '' )
+            b = b.replace ( ' объявления)' , '' )
+            b = b.replace ( ' объявление)' , '' )
+
+        bot.send_message(user_id, "Найдено " + b + " новых товаров за 24 часа" )
+        collichestvo = 0
+        collichestvo = divmod ( int ( b ) , 50 ) [ 0 ]
+        print ( collichestvo )
+        call = 0
+        mass = [ ]
+        for i in range ( collichestvo ) :
+            r = requests.get ( link + "&start=" + str ( call ) )
+            call += 50
+            soup = BS ( r.content , 'html.parser' )
+            for b in soup.findAll ( 'h2' , class_='wraptxt' ) :
+                for a in b.findAll ( 'a' ) :
+                    mass.append ( 'https://baraholka.onliner.by' + a.get ( 'href' ) )
+
+        for i in mass :
+            r = requests.get ( i )
+            soup = BS ( r.content , 'html.parser' )
+
+            for k in soup.findAll ( 'li' , class_='price-primary' ) :
+                bam = re.split ( r',' , k.text ) [ 0 ]
+                if int(sum[0]) <= int(bam) <= int(sum[1]):
+                    print(k.text)
+                    print(i)
+                    for b in soup.findAll('h1', class_='m-title-i title'):
+                        man = b.text
+                    red = i+"\nЦена "+k.text+"\n"+b.text
+                    massiv.append(red)
+
+        red = bot.send_message(user_id, massiv[0], reply_markup=kb.butt)
+        message_id = red.message_id
+        pos[user_id] = 1
+
+@bot.callback_query_handler ( func=lambda call : True )
+def get_call(call) :
+    user_id = call.message.chat.id
+    message_id = call.message.message_id
+    if 'm' in call.data :
+        msg = bot.send_message(chat_id=user_id, text="Вы вернулись в меню.", reply_markup=kb.main)
+        bot.register_next_step_handler(msg, main_room)
+    elif 'd' in call.data:
+        try:
+            bot.edit_message_text(chat_id=user_id, message_id=message_id, text=massiv [ pos [ user_id  ] ])
+            bot.edit_message_reply_markup(chat_id=user_id, message_id=message_id, reply_markup=kb.butt)
+            pos[user_id] += 1
+        except IndexError:
+            pos [ user_id ] = 0
+            bot.send_message(user_id, "Товары в данной ценовой категории закончились")
+            bot.send_message ( user_id , massiv [ pos [ user_id  ] ] , reply_markup=kb.butt )
+
+def waiting(message):
+    user_id = message.chat.id
+    text = message.text
+
+if __name__ == '__main__' :
+    bot.infinity_polling ()
